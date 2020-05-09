a966a9810e52ac7c9e7b36ad1faec7d4773f7060
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-05-09 22:50:49 +0100

    music on start random led

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+music.setVolume(206)
+music.setTempo(61)
+WSTouchPiano.TP_PlayMusic(294, music.beat(BeatFraction.Quarter))
+WSTouchPiano.TP_PlayMusic(262, music.beat(BeatFraction.Quarter))
+WSTouchPiano.TP_PlayMusic(294, music.beat(BeatFraction.Quarter))
+WSTouchPiano.TP_PlayMusic(349, music.beat(BeatFraction.Quarter))
+WSTouchPiano.TP_PlayMusic(294, music.beat(BeatFraction.Quarter))
 basic.forever(function () {
-    WSTouchPiano.TP_PlayPiano()
+    WSTouchPiano.TP_ShowRGB(
+    WSTouchPiano.TP_SetRGB(randint(0, 255), randint(0, 255), randint(0, 255)),
+    WSTouchPiano.TP_SetRGB(randint(0, 255), randint(0, 255), randint(0, 255)),
+    WSTouchPiano.TP_SetRGB(randint(0, 255), randint(0, 255), randint(0, 255)),
+    WSTouchPiano.TP_SetRGB(randint(0, 255), randint(0, 255), randint(0, 255))
+    )
 })
